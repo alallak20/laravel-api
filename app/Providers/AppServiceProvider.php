ef91332251d9b30f8fa7e8b71dca8739b7dfc7c1
alallak20 <bvvgg81@gmail.com>
2024-12-13 18:08:02 +0300

    :hammer: Completed Configuring AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,10 @@ use App\Models\Ticket;
 use App\Models\User;
 use App\Policies\V1\TicketPolicy;
 use App\Policies\V1\UserPolicy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +27,32 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configurePolicies();
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+    }
+
+    private function configurePolicies(): void
+    {
         Gate::policy(Ticket::class, TicketPolicy::class);
         Gate::policy(User::class, UserPolicy::class);
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+    }
+
+    private function configureUrl(): void
+    {
+        URL::forceScheme('https');
     }
 }
